2cb3cb91ac856c3f5085dec74386b6c1da6afe02
Tamar Jafaridze <116025220+nobllese@users.noreply.github.com>
2023-07-03 13:44:02 +0400

    Update main.js

--- 1/main.js
+++ 1/main.js
@@ -1,24 +1,16 @@
-const users = [{
-    name : 'Temo',
-    age: 25
-}, {
-    name: 'Lasha',
-    age: 21
-}, {
-    name: 'Ana',
-    age: 28
-}
+const users = [
+    {name : 'Temo', age: 25},
+    {name: 'Lasha', age: 21},
+    {name: 'Ana',   age: 28}
 ]
 
-let minAge = 9999;
+let minAge = 9999; 
 let name; 
-
 users.forEach((user) => {  
-                          
     if (user.age < minAge){    
         name = user.name;      
         minAge = user.age;
     }
 });
 
-console.log(name); 
+console.log(name);
